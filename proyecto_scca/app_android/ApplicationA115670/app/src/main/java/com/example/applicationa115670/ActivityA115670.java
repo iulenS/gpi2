4fb1699b05ea4e77e4e76b34f334face7688b5d7
Iulen <salinas.115670@e.unavarra.es>
2020-02-19 15:30:27 +0000

    Aplicando gestion de configuracion continua

---
+++ proyecto_scca/app_android/ApplicationA115670/app/src/main/java/com/example/applicationa115670/ActivityA115670.java
@@ -0,0 +1,14 @@
+package com.example.applicationa115670;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityA115670 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
